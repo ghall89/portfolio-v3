f8a1a12198ab681ea51c7da3bf857440db484828
Graham Hall <ghall89@me.com>
2022-10-13 18:21:31 -0400

    fixed build issues

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -2,7 +2,12 @@ import Image from 'next/image';
 
 const FourOhFour = () => (
   <div className="w-fit mx-auto text-center">
-    <Image src="/img/fine-alright.gif" width={220} height={123} />
+    <Image
+      src="/img/fine-alright.gif"
+      width={220}
+      height={123}
+      alt="this is fine"
+    />
     <h3 className="text-2xl">404 Not Found</h3>
   </div>
 );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,8 +5,8 @@ import { P, H3, InlineLink } from '../sharedComponents/Typography';
 const Home = () => (
   <>
     <H3 className="text-center">
-      Hey there! I'm Graham, and I'm a Front-End Web Developer from Rhode
-      Island.
+      Hey there! I&apos;m Graham, and I&apos;m a Front-End Web Developer from
+      Rhode Island.
     </H3>
     <div className="flex flex-col md:flex-row">
       <div className="mx-auto md:mr-9 p-6 min-w-fit">
@@ -20,24 +20,24 @@ const Home = () => (
       <div>
         <P>
           I created my first website when I was in middle school, and even
-          though it wasn't that great it kicked off my love for building great
-          things. All these years later, that passion still burns strong, and my
-          skillset has only grown!
+          though it wasn&apos;t that great it kicked off my love for building
+          great things. All these years later, that passion still burns strong,
+          and my skillset has only grown!
         </P>
         <P>
           I spent several years in the design world, but in 2020 I decided to
           make the jump towards taking on web development professionally. In
           January of 2021, I began attending a web development boot camp with UC
-          Berkeley, and graduated the following summer. Today, I'm a full-time
-          developer at{' '}
+          Berkeley, and graduated the following summer. Today, I&apos;m a
+          full-time developer at{' '}
           <InlineLink href="https://www.nerdpower.energy/">
             Nerd Power
           </InlineLink>
           .
         </P>
         <P>
-          When I'm not writing code you can find me playing video games, hiking,
-          and trying to improve my cooking and art skills!
+          When I&apos;m not writing code you can find me playing video games,
+          hiking, and trying to improve my cooking and art skills!
         </P>
       </div>
     </div>
