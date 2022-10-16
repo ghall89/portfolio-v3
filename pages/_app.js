376dd800b61f69985e8b3fcd624b3202fff32fef
Graham Hall <ghall89@me.com>
2022-10-16 15:17:56 -0400

    animation experiments

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
 import '../styles/globals.css';
 import Layout from '../src/components/Layout';
+import { Router } from 'next/router';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, x: -200, y: 0 }}
+          animate={{ opacity: 1, x: 0, y: 0 }}
+          exit={{ opacity: 0, x: 200, y: 0 }}
+          transition={{ type: 'linear' }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
